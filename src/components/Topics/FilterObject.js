1f2b1371fe5223c9c527d885835d71cf9c939453
Kevin <silencemgo@gmail.com>
2017-09-29 19:36:07 -0400

    filterObject

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,56 @@
 import React, { Component } from 'react';
 
 class FilterObject extends Component {
+
+constructor () {
+    super();
+        this.state={
+            unFilteredArray: [{
+                name:"billy",
+                type: "dog",
+                age:10, 
+                ageInDogYears: 70
+            },
+            {
+                name: "jefferson",
+                type: "bunny",
+                PoopsADay: 39,
+            },
+            {
+                name:"pally",
+                color:"bole/green",
+                age:75,
+            }],
+
+            userInput:"",
+            filteredArray:[]
+
+        }
+    }
+
+
+    getUserInput(e) {
+        this.setState({userInput: e.target.value})
+
+    }
+
+
+    solve() {
+        var filtered = this.state.unfilteredArray.filter((pet)=> pet[this.state.userInput] !== undefined)
+        this.setState({ filteredArray: filtered })
+    }
+
     render() {
         return(
-            <p>FilterObject</p>
+            <div className="puzzleBox filterObjectPB">
+                <h4>Filter Objects</h4>
+                <span className="puzzleText">Original:  [{ unfilteredArray }] </span>
+                <input className="inputLine"
+                        onChange ={(e)=> this.getUserInput(e)}></input>
+                <button className="confirmationButton"
+                onClick = {(e) => this.solve() }>Filter</button>
+                <span className="resultsBox filterObjectRB">Filtered: [{ filteredArray }]</span>
+            </div>
         );
     }
 }
