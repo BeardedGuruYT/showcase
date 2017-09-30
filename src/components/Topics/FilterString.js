c9a8d16c34cddcd6b8a80c52d5a2fe81f2770eb4
Nick Hathaway <Nicholashathaway@gmail.com>
2017-09-29 18:28:37 -0600

    create FilterString

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
 
 class FilterString extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            unFilteredArray: ["London", "Provo", "Christ Church", "Miami", "PSL", "Clearwater", "NYC", "Cincinnati"],
+            userInput: "",
+            filteredArray: []
+        }
+    }
+
+    updateUserInput(e) {
+        this.setState({
+            userInput: e.target.value
+        })
+    }
+
+    solve() {
+        this.setState({
+            filteredArray: this.state.unFilteredArray.filter((str) => str.includes(this.state.userInput))
+        })
+    }
+
     render(){
+        console.log(this.state)
         return(
-            <p>FilterString</p>
+            <div className="puzzleBox filterStringPB">
+                <h4>Filter String</h4>
+                <span className="puzzleText">Cities: [{ this.state.unFilteredArray.map((s)=> " \"" + s + "\", ").join("") }]</span>
+                <input className="inputLine"
+                        onChange={ (e)=> this.updateUserInput(e) }></input>
+                <button className="confirmationButton"
+                        onClick={ (e)=> this.solve() }></button>
+                <span className="resultsBox filterStringRB">Filtered Cities: [{ this.state.filteredArray.map((s)=> " \"" + s + "\", ").join("") }]</span>
+            </div>
         );
     }
 }
